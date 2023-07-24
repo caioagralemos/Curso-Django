871c565a68a7e2d53221b625a752e351c6cb654a
caioagralemos <caioagralemos@live.com>
2023-07-24 20:08:35 -0300

    8 - Accounts & Authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -7,17 +9,52 @@ def dashboard(request):
 
 def login(request):
     if request.method == 'POST':
-        print('submitted request!')
-        return redirect('index')
-    else:
-        return render (request, 'accounts/login.html')
+        username = request.POST['username']
+        password = request.POST['password']
 
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Something went wrong. Check your credentials and try again')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+        
 def logout(request):
-    return redirect('index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out!')
+        return redirect('index')
 
 def register(request):
     if request.method == 'POST':
-        print('submitted request!')
-        return redirect('index')
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password != password2:
+            messages.error(request, 'Passwords do not match!')
+            return redirect('register')
+        elif not User.objects.filter(username=username).exists:
+            messages.error(request, 'Username is already taken')
+            return redirect('register')
+        elif not User.objects.filter(email=email).exists:
+            messages.error(request, 'Email already in use')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+            user.save()
+            messages.success(request, 'You are now registered and can log in!')
+            return redirect('login')
+            # auth.login(request, user)
+            # messages.success(request, 'You are now logged in')
+            # return redirect('index')
     else:
         return render (request, 'accounts/register.html')
