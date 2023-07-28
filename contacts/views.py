a5bea9e56c1aaf7901ba7b8d9574e0f8330d4a3f
caioagralemos <caioagralemos@live.com>
2023-07-28 01:40:15 -0300

    9 - Contact Inquiries

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+from .models import Contacts
 
 # Create your views here.
+
+def contact(request):
+    if request.method == 'POST':
+        listing_id = request.POST['listing_id']
+        listing = request.POST['listing']
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+
+        contact = Contacts.objects.create(listing_id=listing_id, listing=listing, name=name, email=email, phone=phone, message=message, user_id=user_id)
+        contact.save()
+        return redirect('/listings/'+listing_id)
+    else:
+        return redirect('index')
